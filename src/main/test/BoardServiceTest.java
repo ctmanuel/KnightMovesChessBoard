4c9ac9296f233b337c6af1cf5fd765a6e36d63d6
Christian <Christian.T.Manuel@gmail.com>
2018-01-28 11:48:14 -0500

    adding Jar. Updating unit test

--- src/main/test/BoardServiceTest.java
+++ src/main/test/BoardServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class BoardServiceTest {
 
     @Test
@@ -13,6 +15,17 @@ public class BoardServiceTest {
         Location src = new Location(0,7,0);
         Location dest = new Location(7,0,Integer.MAX_VALUE);
         List<Location> moves = boardService.calculateKnightMoves(src,dest);
+        assertEquals(6, moves.size());
+        moves.forEach(move -> System.out.println(move.getX()+","+move.getY()));
+    }
+
+    @Test
+    public void invalidCoordinates(){
+        BoardService boardService = new BoardServiceImpl();
+        Location src = new Location(0,10,0);
+        Location dest = new Location(5,0,Integer.MAX_VALUE);
+        List<Location> moves = boardService.calculateKnightMoves(src,dest);
+        assertEquals(0, moves.size());
         moves.forEach(move -> System.out.println(move.getX()+","+move.getY()));
     }
 }
